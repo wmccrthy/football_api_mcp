0bef9e8addfbed88a6da76489fd737167358dc1d
wmccrthy <wyattmccarthy2020@gmail.com>
2025-03-28 18:00:18 -0400

    added tools for querying players/teams and players/statistics endpoints

--- api_mcp.py
+++ api_mcp.py
@@ -47,7 +47,7 @@ Functions for querying specific endpoints
 """
 
 @mcp.tool()
-async def search_play(name_search: str):
+async def search_player(name_search: str):
     """
     Queries football api players endpoint with a search
 
@@ -59,6 +59,53 @@ async def search_play(name_search: str):
     if not data:
         return f"Unable to fetch player data with search: {name_search}"
     return data
+
+@mcp.tool()
+async def search_player_teams(player_id: str | int):
+    """
+    Queries football api players/team endpoint given a player id
+
+    Args:
+        player_id: player ID as designated by football API (can retrieve via players/profiles query)
+    """
+    player_id = str(player_id) #cast as str in case of int input
+
+    endpoint = f'/players/teams?player={player_id}'
+    data = await make_api_request(endpoint)
+    if not data:
+        return f"Unable to fetch player team data with player ID: {player_id}"
+    return data
+    return
+
+@mcp.tool()
+async def search_player_statistics(player_id: str | int , team_id: str | int, season: str | int):
+    player_id, team_id, season = str(player_id), str(team_id), str(season) #cast as str in case of int input
+    endpoint = f'/players/statistics?player={player_id}'
+    data = await make_api_request(endpoint)
+    if not data:
+        return f"Unable to fetch player statistics data with player ID: {player_id}, team ID: {team_id}, and season: {season}"
+    return data
+
+"""
+For most organized stats retrieval, I'm going to want to use the following players endpoints:
+    - /profiles
+    - /teams
+    These two endpoints to get playerID, then teamIDs and associate seasons with teamIDs
+    Then to actually get statistics we need to query the players/statistics endpoint with the following params: player_id, team_id, season (4digit year)
+    - /statistics
+
+    Assuming we return a list of [(team_id, [seasons])] tuples from our player/teams query, we can have a loop similar to the following:
+    
+        for team_id, seasons in list:
+            for season in seasons:
+                query players/statistics for: player_id, team_id, seasons 
+
+Will probably want a backend function to better parse the retrieved information since it will arrive in an out of sorts manner
+
+
+
+"""
+
 
 @mcp.tool()
 async def search_leagues(name_search: str):
